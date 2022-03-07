ea35c04df2f3b383d483db2ba963e4c6c9121582
Faisal Muaris <faisal.muaris.fm@gmail.com>
2022-03-07 21:00:42 +0700

    menambahkan paragraf

--- index.php
+++ index.php
@@ -8,5 +8,6 @@
 </head>
 <body>
     <h1>Hello, World!</h1>
+    <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Harum dolorem sapiente natus quibusdam, nostrum odit! Consectetur nemo, neque impedit nobis, officia vero repudiandae quos sed ipsum labore modi optio quaerat?</p>
 </body>
 </html>
